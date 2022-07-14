d8c46a99d330a7af0727c65a1c3b996fd3828deb
Joao Coelho <jof.crm@gmail.com>
2022-07-14 15:18:06 -0300

    Exercicios/9.1

--- modulo2-front-end/bloco9-javascript-assync/day1-callbacks/script.js
+++ modulo2-front-end/bloco9-javascript-assync/day1-callbacks/script.js
@@ -56,3 +56,46 @@ sendMarsTemperatureVThree = (callback, errorHandler) => {
 
 setTimeout(() => sendMarsTemperatureVThree(temperatureInFahrenheit, handleError), messageDelay());
 setTimeout(() => sendMarsTemperatureVThree(greet, handleError), messageDelay());
+
+
+// Ex8
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+// getPokemonDetails(
+//   (pokemon) => pokemon.name === 'Squirtle',
+//   (error, message) => error ? console.log(error) : console.log(message));
+
+module.exports = {
+  getPokemonDetails,
+};
--- modulo2-front-end/bloco9-javascript-assync/day1-callbacks/script.test.js
+++ modulo2-front-end/bloco9-javascript-assync/day1-callbacks/script.test.js
@@ -1,7 +1,51 @@
+// const { expect } = require("expect");
+// const { describe, test } = require("jest-circus");
+
 const uppercase = (str, callback) => {
   setTimeout(() => {
     callback(str.toUpperCase());
   }, 500);
 };
 
-describe();
+describe('exercise 7: test to upper case function', () => {
+  test('make sure async functions does not give false-positive results', (done) => {
+    uppercase('test', (str) => {
+      try {
+        expect(str).toBe('TEST');
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  });
+});
+
+const { getPokemonDetails } = require("./script.js");
+
+// getPokemonDetails(
+//   (pokemon) => pokemon.name === 'Bulbasaur',
+//   (error, message) => error ? console.log(error) : console.log(message));
+
+describe('exercise 9, async function testing', () => {
+  test('error returned when pokemon not on database', (done) => {
+    const expectedError = new Error('Não temos esse pokémon para você :(');
+
+    getPokemonDetails(
+      (pokemon) => pokemon.name === 'Squortle',
+      (error) => {
+        expect(error).toEqual(expectedError);
+        done();
+      });
+  });
+
+  test('pokemon returned when it exists on database', (done) => {
+    const expectedMessage = 'Olá, seu pokémon é o Squirtle, o tipo dele é Water e a habilidade principal dele é Water Gun';
+
+    getPokemonDetails(
+      (pokemon) => pokemon.name === 'Squirtle',
+      (error, message) => {
+        expect(message).toBe(expectedMessage);
+        done();
+      });
+  });
+});
